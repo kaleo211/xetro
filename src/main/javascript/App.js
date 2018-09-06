9aca34964786e5cb2689da68d1466fd887d2e98c
Xuebin He <hexuebin0201@gmail.com>
2018-09-06 14:37:34 -0400

    Set welcome page to be active boards list when there are multiple active boards

--- src/main/javascript/App.js
+++ src/main/javascript/App.js
@@ -7,6 +7,7 @@ import Slide from '@material-ui/core/Slide';
 
 import NewBoard from './components/board/NewBoard';
 import Utils from './components/Utils';
+import BoardActiveList from './components/board/BoardActiveList';
 
 const styles = theme => ({
 });
@@ -43,13 +44,7 @@ class App extends React.Component {
       this.setState({ members });
     }));
 
-    this.updateBoards((boards) => {
-      if (boards === null || boards.length === 0) {
-        this.setState({ page: "newBoard" });
-      } else {
-        this.setState({ board: boards[0] });
-      }
-    });
+    this.updateBoards(() => { });
 
     Utils.fetchResource("teams", (body => {
       let teams = body._embedded.teams;
@@ -62,22 +57,35 @@ class App extends React.Component {
     console.log("fetching boards");
     Utils.fetchResource("boards", (body => {
       let boards = body._embedded.boards;
+      if (boards === null || boards.length === 0) {
+        this.setState({ page: "newBoard" });
+      } else if (boards.length === 0) {
+        this.setState({ board: boards[0] });
+        this.setState({ page: "" });
+      } else {
+        this.setState({ page: "activeBoards" });
+      }
       this.setState({ boards }, callback(boards));
     }));
   }
 
   updatePage(page) {
     this.setState({ page });
+    this.updateBoards(() => { });
   }
 
-  updateSelectedBoard(idx) {
+  updateSelectedBoard(boardLink) {
     this.updateBoards((boards) => {
-      this.setState({ board: boards[idx] });
+      boards.map(board => {
+        if (board._links.self.href === boardLink) {
+          this.setState({ board, page: "" });
+        }
+      })
     });
   }
 
   render() {
-    const { members, board, teams } = this.state;
+    const { members, board, teams, boards } = this.state;
     return (
       <div>
         <Board members={members} board={board} />
@@ -85,10 +93,17 @@ class App extends React.Component {
         <Dialog
           fullScreen
           open={this.state.page === "newBoard"}
-          onClose={this.handleSetupPageClose}
           TransitionComponent={Transition}
         >
           <NewBoard members={members} teams={teams} updateSelectedBoard={this.updateSelectedBoard} updatePage={this.updatePage} />
+        </Dialog>
+
+        <Dialog
+          fullScreen
+          open={this.state.page === "activeBoards"}
+          TransitionComponent={Transition}
+        >
+          <BoardActiveList boards={boards} updateSelectedBoard={this.updateSelectedBoard} updatePage={this.updatePage} />
         </Dialog>
       </div >
     );
--- src/main/javascript/components/board/BoardActiveList.js
+++ src/main/javascript/components/board/BoardActiveList.js
@@ -6,6 +6,15 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import MeetingRoomOutlined from '@material-ui/icons/MeetingRoomOutlined';
 
 const styles = theme => ({
 });
@@ -15,17 +24,42 @@ class ActiveBoardList extends React.Component {
     super(props);
   }
 
+  handleBoardSelect(board) {
+    this.props.updateSelectedBoard(board._links.self.href);
+  }
+
   render() {
     const { boards } = this.props;
-    return (
-      <List>
-        {boards && boards.map(board => (
-          <ListItem>
-            <Avatar>{board.team.name}</Avatar>
-            <ListItemText primary={board.name} secondary="created time" />
-          </ListItem>
-        ))}
-      </List>
+    console.log("boards in active list:", boards);
+    return (<div>
+      <AppBar style={{ position: 'relative', }}>
+        <Toolbar>
+          <Typography variant="title" color="inherit" style={{ flex: 1 }} >
+            Active Meetings
+          </Typography>
+          <Button color="inherit" onClick={this.props.updatePage} >Skip</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" alignItems="center"  >
+        <Grid item xs={6} md={4} >
+          <List>
+            {boards && boards.map(board => (
+              <ListItem key={board._links.self.href} dense button >
+                <Avatar>{board.team.name}</Avatar>
+                <ListItemText primary={board.name} />
+                <ListItemSecondaryAction onClick={this.handleBoardSelect.bind(this, board)}>
+                  <IconButton>
+                    <MeetingRoomOutlined />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+
+    </div>
+
     );
   }
 }
