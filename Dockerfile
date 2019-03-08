ef6c14dbc657d488d83fda045330b3dca37dc0fd
Xuebin He <hexuebin0201@gmail.com>
2019-03-07 20:53:22 -0500

    Delete java releated code

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM java:8-jdk
-
-ADD  target/retro-1.0-SNAPSHOT.jar /xetro/xetro.jar
-
-ENTRYPOINT ["java", "-jar", "/xetro/xetro.jar"]
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var server = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var session = require('express-session');
+
+server.use(session({
+  secret: 'Xetro',
+  resave: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+}));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cookieParser())
+
+
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+server.use(express.static('dist'))
+
 
 var port = process.env.PORT || 8080;
-
-var router = express.Router();
-
-router.get('/', function (req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+server.listen(port, () => {
+  console.log('Magic happens on port ' + port);
 });
 
-app.use('/api', router);
-
-app.listen(port);
-console.log('Magic happens on port ' + port);
+module.exports = server;
